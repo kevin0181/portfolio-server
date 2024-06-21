9582db01791a0f57ab6336e8e6bc308ceef29679
unknown <kevin018111@gmail.com>
2024-06-21 23:48:59 +0900

    start

---
+++ src/main/java/com/portfolio/p_server/PServerApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.p_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/p_server/PServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.p_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
